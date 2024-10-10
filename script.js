4f8c321e800dc75943b04dca043422c6498e7412
narong-big <148519918+narong-big@users.noreply.github.com>
2024-10-10 20:57:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,35 +3,47 @@ var count = document.getElementById("score");
 var score = 0;
 var audio = new Audio('pop.mp3');
 
+// Preload audio to ensure it is ready to play
+audio.preload = 'auto';
+
 // mouseclick event
 img.addEventListener("mousedown", function(){
-    // console.log(1, 'mousedown');
     increaseScore();
     img.src = 'popca2.png';
-    audio.play();
+    playAudio();
 });
-    
+
 img.addEventListener("mouseup", function(){
-    // console.log(2, 'mouseup');
     img.src = 'popca1.png';
-    audio.play();
+    playAudio();
 });
 
-// touch event
+// touch event for mobile
 img.addEventListener("touchstart", function(){
-    // console.log(3, 'touchstart');
     increaseScore();
-    img.src = 'popca1.png';
-    audio.play();
+    img.src = 'popca2.png'; // make sure this changes the image
+    playAudio();
 });
 
-img.addEventListener("touchmove", function(){
-    // console.log(4, 'touchmove');
-    img.src = 'popca1.png';
-     audio.play();
+img.addEventListener("touchend", function(){
+    img.src = 'popca1.png'; // change back to original image
+    playAudio();
 });
 
+// Add a fallback for touchcancel (in case the touch is interrupted)
+img.addEventListener("touchcancel", function(){
+    img.src = 'popca1.png'; // reset the image in case of touch cancel
+});
+
+// Function to increase score
 function increaseScore(){
     score++;
     count.innerHTML = score;
+}
+
+// Function to play audio with error handling
+function playAudio() {
+    audio.play().catch(function(error) {
+        console.warn("Audio playback failed:", error);
+    });
 }
